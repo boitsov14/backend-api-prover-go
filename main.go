6efc403451802005f0a783062378fcac726c3f4b
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2025-08-23 14:40:50 +0900

    wip

--- main.go
+++ main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(recover.New())
+	app.Use(helmet.New())
+	app.Use(logger.New())
+	app.Use(compress.New())
+	app.Use(healthcheck.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
