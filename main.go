64d1f3aca6ccef36d8583b7e6a169cbcbf25a60d
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2025-09-09 22:44:00 +0900

    wip

--- main.go
+++ main.go
@@ -46,9 +46,6 @@ func main() {
 		DisableStartupMessage: true,
 	})
 
-	// validator instance
-	validate := validator.New()
-
 	// recover from panics
 	app.Use(recover.New())
 	// security
@@ -68,110 +65,7 @@ func main() {
 	}))
 
 	// main API
-	app.Post("/", func(c *fiber.Ctx) error {
-		log.Info("Received request")
-
-		// initialize request
-		req := new(Request)
-
-		// parse
-		if err := c.BodyParser(req); err != nil {
-			log.Error(err)
-			return c.SendStatus(fiber.StatusBadRequest)
-		}
-		log.Info(req)
-
-		// validate
-		if err := validate.Struct(req); err != nil {
-			log.Error(err)
-			return c.SendStatus(fiber.StatusBadRequest)
-		}
-		log.Info("Validation passed")
-
-		// temporary directory
-		out, err := os.MkdirTemp(".", "out-")
-		if err != nil {
-			log.Error(err)
-			return c.SendStatus(fiber.StatusInternalServerError)
-		}
-		// cleanup
-		defer func() {
-			if err := os.RemoveAll(out); err != nil {
-				log.Error(err)
-			} else {
-				log.Info("Cleaned up temporary directory:", out)
-			}
-		}()
-		log.Info("Created temporary directory:", out)
-
-		// write formula to file
-		if err := os.WriteFile(filepath.Join(out, "formula.txt"), []byte(req.Formula), PERM); err != nil {
-			log.Error(err)
-			return c.SendStatus(fiber.StatusInternalServerError)
-		}
-		log.Info("Wrote formula to file")
-
-		// write options to file
-		if err := os.WriteFile(filepath.Join(out, "options.json"), []byte(req.Options), PERM); err != nil {
-			log.Error(err)
-			return c.SendStatus(fiber.StatusInternalServerError)
-		}
-		log.Info("Wrote options to file")
-
-		// context with timeout
-		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(req.Timeout)*time.Second)
-		defer cancel()
-
-		// execute prover
-		cmd := exec.CommandContext(ctx, "./prover", "--out", out) // #nosec G204
-		output, err := cmd.CombinedOutput()
-		// check if timed out
-		timeout := errors.Is(ctx.Err(), context.DeadlineExceeded)
-		switch {
-		case timeout:
-			log.Warn("Timeout")
-		case err != nil:
-			log.Error(err)
-		default:
-			log.Info("Completed successfully")
-		}
-
-		// initialize response
-		response := Response{
-			Output:  string(output),
-			Timeout: timeout,
-			Files:   make(map[string]string),
-		}
-
-		// read all files from output directory
-		files, err := os.ReadDir(out)
-		if err != nil {
-			log.Error(err)
-			// return response without files
-			return c.JSON(response)
-		}
-		log.Info("Found", len(files), "files in output directory")
-
-		// process each file in output directory
-		for _, file := range files {
-			filename := file.Name()
-
-			// read file
-			content, err := os.ReadFile(filepath.Join(out, filename)) // #nosec G304
-			if err != nil {
-				log.Error(err)
-				// skip and continue
-				continue
-			}
-
-			// add content to response
-			response.Files[filename] = string(content)
-			log.Info("Added file:", filename)
-		}
-
-		// return response
-		return c.JSON(response)
-	})
+	app.Post("/", prove)
 
 	// initialize port
 	port := os.Getenv("PORT")
@@ -182,4 +76,112 @@ func main() {
 	// start server
 	log.Info("Starting server on port:", port)
 	log.Fatal(app.Listen(":" + port))
+}
+
+func prove(c *fiber.Ctx) error {
+	log.Info("Received request")
+
+	// validator instance
+	validate := validator.New()
+
+	// initialize request
+	req := new(Request)
+
+	// parse
+	if err := c.BodyParser(req); err != nil {
+		log.Error(err)
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	log.Info(req)
+
+	// validate
+	if err := validate.Struct(req); err != nil {
+		log.Error(err)
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	log.Info("Validation passed")
+
+	// temporary directory
+	out, err := os.MkdirTemp(".", "out-")
+	if err != nil {
+		log.Error(err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	// cleanup
+	defer func() {
+		if err := os.RemoveAll(out); err != nil {
+			log.Error(err)
+		} else {
+			log.Info("Cleaned up temporary directory:", out)
+		}
+	}()
+	log.Info("Created temporary directory:", out)
+
+	// write formula to file
+	if err := os.WriteFile(filepath.Join(out, "formula.txt"), []byte(req.Formula), PERM); err != nil {
+		log.Error(err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	log.Info("Wrote formula to file")
+
+	// write options to file
+	if err := os.WriteFile(filepath.Join(out, "options.json"), []byte(req.Options), PERM); err != nil {
+		log.Error(err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	log.Info("Wrote options to file")
+
+	// context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(req.Timeout)*time.Second)
+	defer cancel()
+
+	// execute prover
+	cmd := exec.CommandContext(ctx, "./prover", "--out", out) // #nosec G204
+	output, err := cmd.CombinedOutput()
+	// check if timed out
+	timeout := errors.Is(ctx.Err(), context.DeadlineExceeded)
+	switch {
+	case timeout:
+		log.Warn("Timeout")
+	case err != nil:
+		log.Error(err)
+	default:
+		log.Info("Completed successfully")
+	}
+
+	// initialize response
+	response := Response{
+		Output:  string(output),
+		Timeout: timeout,
+		Files:   make(map[string]string),
+	}
+
+	// read all files from output directory
+	files, err := os.ReadDir(out)
+	if err != nil {
+		log.Error(err)
+		// return response without files
+		return c.JSON(response)
+	}
+	log.Info("Found", len(files), "files in output directory")
+
+	// process each file in output directory
+	for _, file := range files {
+		filename := file.Name()
+
+		// read file
+		content, err := os.ReadFile(filepath.Join(out, filename)) // #nosec G304
+		if err != nil {
+			log.Error(err)
+			// skip and continue
+			continue
+		}
+
+		// add content to response
+		response.Files[filename] = string(content)
+		log.Info("Added file:", filename)
+	}
+
+	// return response
+	return c.JSON(response)
 }
